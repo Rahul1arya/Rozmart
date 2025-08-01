f8db44fc0f6a840a679f23d5a098e72236084c4e
Rahul1arya <rahularyashabga@gmail.com>
2025-08-01 06:47:35 +0530

    Fix: saved user model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,9 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String, // for simplicity only
-});
-
-module.exports = mongoose.model("User", UserSchema);
-const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
 });
+
+module.exports = mongoose.model("User", UserSchema);
