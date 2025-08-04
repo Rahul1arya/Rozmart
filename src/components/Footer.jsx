f2a0e1e46809a42a2d50468fcdb5abb086e944fd
Rahul1arya <rahularyashabga@gmail.com>
2025-08-04 06:39:37 +0530

    Deleted all files

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white p-4 text-center mt-12">
-      © {new Date().getFullYear()} MyShop. All rights reserved.
-    </footer>
-  );
-}
