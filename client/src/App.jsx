e691bd71b39591da3109d262c85dc5c22c442105
Rahul1arya <rahularyashabga@gmail.com>
2025-08-06 21:37:17 +0530

    fix: correct script path in index.html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
-import Home from "./Home";
+import Home from "./pages/Home";
 import Login from "./Login";
 import Checkout from "./Checkout";
 import Admin from "./Admin";
